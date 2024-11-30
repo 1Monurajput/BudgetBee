0d0209346cf53207414645786724aec8bde27973
1Monurajput <rjp7401@gmail.com>
2024-11-30 11:04:14 +0530

    First day

---
+++ expensetracker/src/main/java/com/expensetracker/expensetracker/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.expensetracker.expensetracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(){
+        System.out.println("This is home page........");
+        return "home";
+    }
+}
